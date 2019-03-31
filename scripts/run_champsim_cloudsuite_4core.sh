c085e1d69b97bd0ef0f3618801d74cbc20478e22
Akanksha Jain <akanksha@cs.utexas.edu>
2019-03-30 23:10:10 -0500

    Cloudsuite scripts

---
+++ scripts/run_champsim_cloudsuite_4core.sh
@@ -0,0 +1,11 @@
+TRACE_DIR=/scratch/cluster/akanksha/CloudSuiteTraces/
+binary=${1}
+n_warm=${2}
+n_sim=${3}
+trace=${4}
+outputdir=${5}
+option=${6}
+
+echo "(/u/akanksha/MyChampSim/ChampSim/bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 -cloudsuite -${option} -traces ${TRACE_DIR}/${trace}_core0.trace.gz ${TRACE_DIR}/${trace}_core1.trace.gz ${TRACE_DIR}/${trace}_core2.trace.gz ${TRACE_DIR}/${trace}_core3.trace.gz) &> ${outputdir}/${trace}.txt"
+(/u/akanksha/MyChampSim/ChampSim/bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 -cloudsuite -hide_heartbeat -low_bandwidth -${option} -traces ${TRACE_DIR}/${trace}_core0.trace.gz ${TRACE_DIR}/${trace}_core1.trace.gz ${TRACE_DIR}/${trace}_core2.trace.gz ${TRACE_DIR}/${trace}_core3.trace.gz) &> ${outputdir}/${trace}.txt
+
